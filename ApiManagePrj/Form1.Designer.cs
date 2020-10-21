44689d9a77de761e419698723ee7056a85997350
xjx0106 <xjx0106@126.com>
2020-10-21 19:27:25 +0800

    输入项目地址识别node_modules以及里面的api项目名称

--- ApiManagePrj/Form1.Designer.cs
+++ ApiManagePrj/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(601, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "C:\\Users\\XuJingxin\\Documents\\许景欣的工程文件\\14.Insight中台和句法模板\\2.句法模板\\rule-library";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(670, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(46, 124);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(109, 172);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "api项目名";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ApiManagePrj/Form1.cs
+++ ApiManagePrj/Form1.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,53 @@ namespace ApiManagePrj
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string projectPath = textBox1.Text;
+            // string[] files = Directory.GetFiles(projectPath,"*.*", SearchOption.TopDirectoryOnly);
+            string[] rootFolders = foreachFolders(projectPath);
+            string[] apiFolders;
+            foreach (string folder in rootFolders)
+            {
+                if (folder.EndsWith("node_modules"))
+                {
+                    apiFolders = foreachFolders(projectPath + @"\node_modules\@dataexa");
+                    for (int i = 0; i < apiFolders.Length; i++)
+                    {
+                        int index = apiFolders[i].LastIndexOf(@"\");
+                        apiFolders[i] = apiFolders[i].Substring(index + 1);
+                        if (apiFolders[i].StartsWith("api"))
+                        {
+                            listBox1.Items.Add(apiFolders[i]);
+                        }
+                    }
+                }
+            }
+        }
+
+        public string[] foreachFolders(string path)
+        {
+            string[] folders = Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly);
+            return folders;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string apiList;
+            string[] result;
+            string apiFolderPath = textBox1.Text + @"\node_modules\@dataexa\" + listBox1.SelectedItem.ToString();
+            //\src\client\index.ts
+            string apiListFilePath = apiFolderPath + @"\src\client\index.ts";
+            using (FileStream fs = File.Open(apiListFilePath, FileMode.Open))
+            {
+                StreamReader br = new StreamReader(fs, Encoding.UTF8);
+                Console.WriteLine(br.ReadToEnd());
+                apiList = br.ReadToEnd().Replace("\n", "").Replace(" ", "").Replace("\t", "").Replace("\r", "");
+                Console.WriteLine(apiList);
+                Regex reg = new Regex(@"\Aimport\zfrom");
+            }
         }
     }
 }
