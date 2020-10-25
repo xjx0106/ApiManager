c2755040c9be45398f4fca2203becffb7a4ec4fd
xjx0106 <xjx0106@126.com>
2020-10-25 16:43:29 +0800

    代码优化，添加忽略文件，解析得api类列表

--- ApiManagePrj/Form1.Designer.cs
+++ ApiManagePrj/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -40,7 +44,8 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(601, 21);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "C:\\Users\\XuJingxin\\Documents\\许景欣的工程文件\\14.Insight中台和句法模板\\2.句法模板\\rule-library";
+            this.textBox1.Text = "C:\\Users\\XuJingxin\\Documents\\许景欣的工程文件\\14.Maya迭代3.0\\2.句法模板\\rule-library";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
@@ -54,28 +59,70 @@
             // 
             // listBox1
             // 
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Default;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(46, 124);
+            this.listBox1.Location = new System.Drawing.Point(40, 105);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(109, 172);
+            this.listBox1.Size = new System.Drawing.Size(138, 304);
             this.listBox1.TabIndex = 2;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 98);
+            this.label1.Location = new System.Drawing.Point(38, 79);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 12);
             this.label1.TabIndex = 3;
             this.label1.Text = "api项目名";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "api接口类名";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(197, 105);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(207, 304);
+            this.listBox2.TabIndex = 5;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(432, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "api接口名";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 12;
+            this.listBox3.Location = new System.Drawing.Point(423, 105);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(183, 304);
+            this.listBox3.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 509);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
@@ -93,6 +140,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
     }
 }
 
--- ApiManagePrj/Form1.cs
+++ ApiManagePrj/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,65 +11,144 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace ApiManagePrj
 {
     public partial class Form1 : Form
     {
+        public string projectPath = ""; // 前端项目的地址
+
+        // 公共操作
+        /// <summary>
+        /// 读取文件流
+        /// </summary>
+        /// <param name="path">要读取的文件地址</param>
+        /// <returns></returns>
+        public string readTextFile(string path)
+        {
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                StreamReader br = new StreamReader(fs, Encoding.UTF8);
+                return br.ReadToEnd(); //Replace("\n", "")
+                // Console.WriteLine(apiListToString);
+            }
+        }
+
+        //================================================================================
+        //================================================================================
+        /// <summary>
+        /// 窗口初始化
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
+            projectPath = textBox1.Text; // 获取前端项目的地址
         }
 
+        /// <summary>
+        /// 点击按钮，解析前端项目，在listBox1添加所检索到的api项目名
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            string projectPath = textBox1.Text;
             // string[] files = Directory.GetFiles(projectPath,"*.*", SearchOption.TopDirectoryOnly);
-            string[] rootFolders = foreachFolders(projectPath);
-            string[] apiFolders;
-            foreach (string folder in rootFolders)
+            string[] rootFolders = foreachFolders(projectPath); // 遍历项目里的子文件夹（为了寻找出node_modules）
+            listBox1.Items.Clear(); // 清除api项目列表
+            foreach (string folder in rootFolders) // 遍历根目录找出node_modules文件夹
             {
-                if (folder.EndsWith("node_modules"))
+                if (folder.EndsWith("node_modules")) // 选择出node_modules文件夹
                 {
-                    apiFolders = foreachFolders(projectPath + @"\node_modules\@dataexa");
-                    for (int i = 0; i < apiFolders.Length; i++)
+                    string[] folderInAtDataexa = foreachFolders(projectPath + @"\node_modules\@dataexa");
+                    for (int i = 0; i < folderInAtDataexa.Length; i++) // 遍历node_modules\@dataexa
                     {
-                        int index = apiFolders[i].LastIndexOf(@"\");
-                        apiFolders[i] = apiFolders[i].Substring(index + 1);
-                        if (apiFolders[i].StartsWith("api"))
+                        int index = folderInAtDataexa[i].LastIndexOf(@"\");
+                        folderInAtDataexa[i] = folderInAtDataexa[i].Substring(index + 1);
+                        if (folderInAtDataexa[i].StartsWith("api"))
                         {
-                            listBox1.Items.Add(apiFolders[i]);
+                            listBox1.Items.Add(folderInAtDataexa[i]); // 检测出以api开头的文件夹，添加。
                         }
                     }
+                    
                 }
             }
         }
 
+        /// <summary>
+        /// 遍历该路径下的文件夹
+        /// </summary>
+        /// <param name="path">要遍历的文件夹路径</param>
+        /// <returns>返回一个string[]</returns>
         public string[] foreachFolders(string path)
         {
-            string[] folders = Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly);
-            return folders;
+            try
+            {
+                string[] folders = Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly);
+                return folders;
+            }
+            catch
+            {
+                MessageBox.Show("没有找到该文件夹");
+                return null;
+            }
         }
 
+        /// <summary>
+        /// 点击listBox1里的项目，遍历该Api项目的api类名
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string apiListRead;
-            string apiListToString;
             string apiFolderPath = textBox1.Text + @"\node_modules\@dataexa\" + listBox1.SelectedItem.ToString();
-            //\src\client\index.ts
-            string apiListFilePath = apiFolderPath + @"\src\client\index.ts";
-            using (FileStream fs = File.Open(apiListFilePath, FileMode.Open))
-            {
-                StreamReader br = new StreamReader(fs, Encoding.UTF8);
-                apiListRead = br.ReadToEnd(); //Replace("\n", "")
-                apiListToString = apiListRead.Replace("\n","").Replace(" ", "").Replace("\t", "").Replace("\r", "");
-                //Console.WriteLine(apiListToString);
-            }
+            string apiClassListFilePath = apiFolderPath + @"\src\client\index.ts"; // 要读取Api类名的目标文件
+            string apiClassListRead; // 所读取到的api项目里的包含api类名的那个ts文件，返回带有回车和tab的string
+            string apiListClassToString; // 所读取到的api项目里的包含api类名的那个ts文件，去掉了回车空格之类的
+
+            apiClassListRead = readTextFile(apiClassListFilePath);
+            apiListClassToString = apiClassListRead.Replace("\n", "").Replace(" ", "").Replace("\t", "").Replace("\r", "");
+
             string start = "import{";
             string end = "}from";
             Regex rg = new Regex("(?<=(" + start + "))[.\\s\\S]*?(?=(" + end + "))", RegexOptions.Multiline | RegexOptions.Singleline);
-            Console.WriteLine("apis: " + rg.Match(apiListToString).Value);
+            // Console.WriteLine("apis: " + rg.Match(apiListToString).Value);
 
+            string[] apiClasses = rg.Match(apiListClassToString).Value.Split(','); // 将正则所匹配到的"WordApi,WordNetApi,StrategyApi,"匹配逗号分离开，存入apiClasses
+            listBox2.Items.Clear();
+            foreach (string item in apiClasses)
+            {
+                if (item != "")
+                {
+                    listBox2.Items.Add(item);
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// listBox2项被点击的事件触发
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string path = projectPath + @"\node_modules\@dataexa\"+ listBox1.SelectedItem + @"\src\api\api.ts"; 
+            Console.WriteLine(path);
+            string apiListFileRead = readTextFile(path);
+
+            string pattren1 = listBox2.SelectedItem.ToString(); // WordApi
+        }
+
+        /// <summary>
+        /// 项目地址输入框文字更变的事件触发
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            projectPath = textBox1.Text;
         }
     }
 }
